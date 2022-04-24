d5e0e599293d607720867d1cb55bdb37dc26f758
sadasiva111 <sadasiva6699@gmail.com>
2022-04-24 20:41:25 +0200

    Test Cases With Correct Functionality

--- String_Methods_Application.py
+++ String_Methods_Application.py
@@ -5,18 +5,17 @@ class string_methods_application:
     #Author : Boya Sada Siva Kumar
     #converts the first letter of the string to capital letter.
     def capitalize_string(my_string):
-        return 0
+        return my_string.capitalize()
 
     #Author : Boya Sada Siva Kuma
     #Returns true if the start value is specified value
     def startswith_string(my_string):
-        return 0
+        return my_string.startswith("Software")
 
     #Author : Boya Sada Siva Kuma
     #Replaces the old string with new string
     def replace_string(my_string):
-        return 0
+        return my_string.replace("software", "unit")
         
         
-
         
