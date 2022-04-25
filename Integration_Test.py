f25af945a4b8a9e3a819233262804416b67c834e
Vamsi-Teeparthi <vamsi5256@gmail.com>
2022-04-25 15:45:48 +0200

    Integration test by vamsi

--- Integration_Test.py
+++ Integration_Test.py
@@ -13,3 +13,9 @@ class TestStringMethods(unittest.TestCase):
         output = string_methods_application.startswith_string(string_methods_application.capitalize_string("software testing."))
         self.assertTrue(output)
 
+    #Author : Vamsi Krishna Teeparthi
+    #Integration Test Case 2
+    #Integrating endswith and casefold string methods
+    def test_integration_Task_2(self):
+        output = string_methods_application.endswith_string(string_methods_application.casefold_string("SOFTWARE TESTING."))
+        self.assertTrue(output, ".")
